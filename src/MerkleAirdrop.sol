bad948696e777033f2952627bff0adf34b69344d
LordDregg <oluwatosinodejide121@gmail.com>
2026-03-11 07:34:15 +0100

    prevent double-claiming

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -9,6 +9,7 @@ contract MerkleAirdrop {
 
   // Errors
   error Airdrop__InvalidProof();
+  error Airdrop_AlreadyClaimed();
 
   // Events
   event Airdrop_Claimed(address account, uint256 amount);
@@ -17,6 +18,7 @@ contract MerkleAirdrop {
   bytes32 immutable i_merkleRoot;
 
   address[] claimers;
+  mapping(address claimer => bool claimed) private hasClaimed;
 
   constructor(IERC20 _airdropToken, bytes32 _merkleRoot) {
     i_merkleRoot = _merkleRoot;
@@ -27,7 +29,10 @@ contract MerkleAirdrop {
     bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(_account, _amount))));
 
     if (!MerkleProof.verify(_merkleProof, i_merkleRoot, leaf)) revert Airdrop__InvalidProof();
+    if (hasClaimed[msg.sender]) revert Airdrop_AlreadyClaimed();
 
+    hasClaimed[msg.sender] = true;
+ 
     emit Airdrop_Claimed(_account, _amount);
 
     i_token.safeTransfer(_account, _amount);
